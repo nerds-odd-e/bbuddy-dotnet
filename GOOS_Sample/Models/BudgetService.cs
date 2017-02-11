45a905bd1dfddf07df01c42f323c8f4b83d69370
hatelove <jay.hatelove@gmail.com>
2017-02-11 16:02:24 +0800

    pass BudgetService.Create() isolated unit test

--- GOOS_Sample/Models/BudgetService.cs
+++ GOOS_Sample/Models/BudgetService.cs
@@ -15,13 +15,8 @@ namespace GOOS_Sample.Models
 
         public void Create(BudgetAddViewModel model)
         {
-            using (var dbcontext = new NorthwindEntities())
-            {
-                var budget = new Budget() { Amount = model.Amount, YearMonth = model.Month };
-                dbcontext.Budgets.Add(budget);
-
-                dbcontext.SaveChanges();
-            }
+            var budget = new Budget() {Amount = model.Amount, YearMonth = model.Month};
+            this._budgetRepository.Save(budget);
         }
     }
 }
