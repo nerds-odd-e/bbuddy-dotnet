d4e18a843030da885951290aee1fbff9a5490bbb
hatelove <jay.hatelove@gmail.com>
2017-02-12 01:19:43 +0800

    refactor testing: BudgetServiceTests

--- GOOS_SampleTests/Models/BudgetServiceTests.cs
+++ GOOS_SampleTests/Models/BudgetServiceTests.cs
@@ -12,13 +12,13 @@ namespace GOOS_Sample.Models.Tests
     [TestClass()]
     public class BudgetServiceTests
     {
-        private BudgetService _budgetService;
         private IRepository<Budget> _budgetRepositoryStub = Substitute.For<IRepository<Budget>>();
+        private BudgetService _budgetService;
 
         [TestMethod()]
         public void CreateTest_should_invoke_repository_one_time()
         {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            InjectStubToBudgetService();
 
             var wasCreated = false;
             this._budgetService.Created += (sender, e) => { wasCreated = true; };
@@ -37,7 +37,7 @@ namespace GOOS_Sample.Models.Tests
         [TestMethod()]
         public void CreateTest_when_exist_record_should_update_budget()
         {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            InjectStubToBudgetService();
 
             var wasUpdated = false;
             this._budgetService.Updated += (sender, e) => { wasUpdated = true; };
@@ -56,10 +56,36 @@ namespace GOOS_Sample.Models.Tests
             Assert.IsTrue(wasUpdated);
         }
 
+        [TestMethod]
+        public void TotalBudgetTest_Period_EndDate_over_month_when_two_months_budget()
+        {
+            InjectStubToBudgetService();
+
+            _budgetRepositoryStub.ReadAll()
+                .ReturnsForAnyArgs(new List<Budget>
+                {
+                    new Budget() { YearMonth = "2017-04", Amount = 9000 },
+                    new Budget() { YearMonth = "2017-05", Amount = 3100 },
+                });
+
+            AssertTotalAmount(9500, new Period(new DateTime(2017, 4, 1), new DateTime(2017, 5, 5)));
+        }
+
+        [TestMethod]
+        public void TotalBudgetTest_Period_EndDate_over_single_month_but_only_one_month_budget()
+        {
+            InjectStubToBudgetService();
+
+            _budgetRepositoryStub.ReadAll()
+                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
+
+            AssertTotalAmount(3000, new Period(new DateTime(2017, 4, 21), new DateTime(2017, 5, 10)));
+        }
+
         [TestMethod()]
         public void TotalBudgetTest_Period_of_single_month()
         {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            InjectStubToBudgetService();
 
             _budgetRepositoryStub.ReadAll()
                 .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
@@ -72,92 +98,9 @@ namespace GOOS_Sample.Models.Tests
         }
 
         [TestMethod]
-        public void TotalBudgetTest_Period_StartDate_over_single_month_but_only_one_month_budget()
-        {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
-
-            _budgetRepositoryStub.ReadAll()
-                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
-
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 3, 5), new DateTime(2017, 4, 10)));
-
-            var expected = 3000;
-
-            amount.ShouldBeEquivalentTo(expected);
-        }
-
-        [TestMethod]
-        public void TotalBudgetTest_Period_EndDate_over_single_month_but_only_one_month_budget()
-        {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
-
-            _budgetRepositoryStub.ReadAll()
-                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
-
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 4, 21), new DateTime(2017, 5, 10)));
-
-            var expected = 3000;
-
-            amount.ShouldBeEquivalentTo(expected);
-        }
-
-        [TestMethod]
-        public void TotalBudgetTest_Period_over_single_month_but_only_one_month_budget()
-        {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
-
-            _budgetRepositoryStub.ReadAll()
-                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
-
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 3, 21), new DateTime(2017, 5, 10)));
-
-            var expected = 9000;
-
-            amount.ShouldBeEquivalentTo(expected);
-        }
-
-        [TestMethod]
-        public void TotalBudgetTest_Period_StartDate_over_month_when_two_months_budget()
-        {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
-
-            _budgetRepositoryStub.ReadAll()
-                .ReturnsForAnyArgs(new List<Budget>
-                {
-                    new Budget() { YearMonth = "2017-03", Amount = 3100 },
-                    new Budget() { YearMonth = "2017-04", Amount = 9000 }
-                });
-
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 3, 22), new DateTime(2017, 4, 30)));
-
-            var expected = 10000;
-
-            amount.ShouldBeEquivalentTo(expected);
-        }
-
-        [TestMethod]
-        public void TotalBudgetTest_Period_EndDate_over_month_when_two_months_budget()
-        {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
-
-            _budgetRepositoryStub.ReadAll()
-                .ReturnsForAnyArgs(new List<Budget>
-                {
-                    new Budget() { YearMonth = "2017-04", Amount = 9000 },
-                    new Budget() { YearMonth = "2017-05", Amount = 3100 },
-                });
-
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 4, 1), new DateTime(2017, 5, 5)));
-
-            var expected = 9500;
-
-            amount.ShouldBeEquivalentTo(expected);
-        }
-
-        [TestMethod]
         public void TotalBudgetTest_Period_over_month_when_3_months_budget()
         {
-            this._budgetService = new BudgetService(_budgetRepositoryStub);
+            InjectStubToBudgetService();
 
             _budgetRepositoryStub.ReadAll()
                 .ReturnsForAnyArgs(new List<Budget>
@@ -167,11 +110,54 @@ namespace GOOS_Sample.Models.Tests
                     new Budget() { YearMonth = "2017-05", Amount = 3100 },
                 });
 
-            var amount = this._budgetService.TotalBudget(new Period(new DateTime(2017, 3, 22), new DateTime(2017, 5, 5)));
+            AssertTotalAmount(11500, new Period(new DateTime(2017, 3, 22), new DateTime(2017, 5, 5)));
+        }
 
-            var expected = 11500;
+        [TestMethod]
+        public void TotalBudgetTest_Period_over_single_month_but_only_one_month_budget()
+        {
+            InjectStubToBudgetService();
 
-            amount.ShouldBeEquivalentTo(expected);
+            _budgetRepositoryStub.ReadAll()
+                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
+
+            AssertTotalAmount(9000, new Period(new DateTime(2017, 3, 21), new DateTime(2017, 5, 10)));
+        }
+
+        [TestMethod]
+        public void TotalBudgetTest_Period_StartDate_over_month_when_two_months_budget()
+        {
+            InjectStubToBudgetService();
+
+            _budgetRepositoryStub.ReadAll()
+                .ReturnsForAnyArgs(new List<Budget>
+                {
+                    new Budget() { YearMonth = "2017-03", Amount = 3100 },
+                    new Budget() { YearMonth = "2017-04", Amount = 9000 }
+                });
+
+            AssertTotalAmount(10000, new Period(new DateTime(2017, 3, 22), new DateTime(2017, 4, 30)));
+        }
+
+        [TestMethod]
+        public void TotalBudgetTest_Period_StartDate_over_single_month_but_only_one_month_budget()
+        {
+            InjectStubToBudgetService();
+
+            _budgetRepositoryStub.ReadAll()
+                .ReturnsForAnyArgs(new List<Budget> { new Budget() { YearMonth = "2017-04", Amount = 9000 } });
+
+            AssertTotalAmount(3000, new Period(new DateTime(2017, 3, 5), new DateTime(2017, 4, 10)));
+        }
+
+        private void AssertTotalAmount(int expected, Period period)
+        {
+            this._budgetService.TotalBudget(period).ShouldBeEquivalentTo(expected);
+        }
+
+        private void InjectStubToBudgetService()
+        {
+            this._budgetService = new BudgetService(_budgetRepositoryStub);
         }
     }
 }
